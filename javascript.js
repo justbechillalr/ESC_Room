8e845c3e3909cf5c5507c9f6cd6ec5ba02b6d221
justbechillalr <abdullahihusseinali123gmail.com>
2026-05-15 11:18:21 +0200

    Mistet js fil til ny branch men lagde ny så den kommer til main

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function toggleKeyRoom1(){
+    
+    document.getElementById("key1-inventory").classList.toggle("hidden");
+    document.getElementById("key1-click").classList.toggle("hidden");
+    document.getElementById("room2").href = "Room_2.html"
+}
